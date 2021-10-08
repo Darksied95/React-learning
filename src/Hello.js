13bd864e8b03e4f8a993b2211acc24216ab59659
Darksied95 <aliyuefe9501@gmail.com>
2021-10-08 21:10:17 +0100

    done

--- src/Hello.js
+++ src/Hello.js
@@ -4,7 +4,12 @@ import './Hello.css';
 
 class Hello extends Component{
     render(){
-        return <h1>Hello world</h1>
+        return(
+        <div class='f1 tc'>
+            <h1>Hello world</h1>;
+            <h2>I'm react</h2>;
+            </div>
+         )
     }
 }
 export default Hello;
