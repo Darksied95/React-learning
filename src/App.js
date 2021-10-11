9c1fa9be8e26bcc5f53ae0e14b9ed2d9ff77e7b1
Darksied95 <aliyuefe9501@gmail.com>
2021-10-11 06:22:57 +0100

    we made it scrollable and added a scroll folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import Searchbox from './Searchbox';
 import "./App.css";
+import Scroll from './Scroll';
  
 class App extends React.Component{
     constructor(){
         super()
         this.state={
-            robots:robots,
+            robots:[],
             searchfield:""
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> {
+            return response.json();
+        })
+        .then(users=> {
+            this.setState({ robots: users})
+        });
     }
     onSearchChange=(event)=>{
         this.setState({searchfield:event.target.value});
@@ -20,13 +30,21 @@ class App extends React.Component{
         const filteredRobots=this.state.robots.filter(robots=>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
             })
-    return(
-        <div className="tc">
-        <h1 className="f2">Robofriends</h1>
-        <Searchbox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots}/>
-        </div>
-    )
+
+            if (this.state.robots.length===0){
+                return <h1>Loading</h1>
+            }else{
+                return(
+                    <div className="tc">
+                    <h1 className="f2">Robofriends</h1>
+                    <Searchbox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                    <CardList robots={filteredRobots}/>
+                    </Scroll>
+                    </div>
+                )
+            }
+   
  }
 }
 
